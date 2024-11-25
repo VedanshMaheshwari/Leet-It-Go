599ef178ea897dab7d253a5f3bc542be4e4473a6
VedanshMaheshwari <vedanshm15@gmail.com>
2024-11-25 22:35:32 +0530

    191

---
+++ Basic Concepts/DtoB.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int decimalToBinary(int n) {
+    int number = 0;  
+
+    int i = 0;       
+
+    while (n != 0) {
+        int digit = n & 1;           // Extract the least significant bit
+        number += digit * pow(10, i); // Use addition instead of direct assignment
+        n = n >> 1;                  // Right shift to move to the next bit
+        i++;
+    }
+
+    return number;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int ans = 0;
+    int i = 0;
+    while(n != 0){
+        int bit = n & 1;
+
+        ans = (bit * pow(10, i)) + ans;
+
+        n = n >> 1;
+        i ++;
+    }
+
+    cout<< ans << endl;
+}
