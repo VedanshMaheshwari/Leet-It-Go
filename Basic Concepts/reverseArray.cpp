3a4078f7c46cced881722c3ed8867d63a76dd37f
VedanshMaheshwari <vedanshm15@gmail.com>
2024-11-28 11:41:03 +0530

    Array Qs

---
+++ Basic Concepts/reverseArray.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+
+void reverseArray(int array[],int size){
+        int temp = 0;
+        for (int i = 0; i < size/2; i++)
+        {
+            temp = array[i];
+            array[i] = array[size-1-i];
+            array[size-1-i] = temp;
+        }
+}
+
+void reverse(int array[], int size){
+   
+    int first = 0;
+    int last = size -1;
+    while(first <= last){
+        swap(array[first], array[last]);
+        first++;
+        last--;
+    }
+}
+
+int main(){
+
+    int numOfElements;
+    cin >> numOfElements;
+
+    int array[numOfElements];
+
+    for (size_t i = 0; i < numOfElements; i++)
+    {
+        cin >> array[i];
+    }
+    
+    reverseArray(array,numOfElements);
+    reverse(array, numOfElements);
+
+    for (size_t i = 0; i < numOfElements; i++)
+    {
+        cout << array[i] << " ";
+    }
+    
+
+}
---
+++ Basic Concepts/tempCodeRunnerFile.cpp
@@ -0,0 +1,20 @@
+void reverseArray(int array[],int size){
+
+//     if((size % 2) == 0){
+//         int temp = 0;
+//         for (int i = 0; i < size/2; i++)
+//         {
+//             temp = array[i];
+//             array[i] = array[size-1];
+//             array[size-1] = temp;
+
+//             size--;  
+//         }
+
+//         return array[];
+//     }
+//     else{
+
+//     }
+
+// }
