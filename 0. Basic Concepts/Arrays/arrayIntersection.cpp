a3c408129b67ac81ef3137bb80d7657dae860344
VedanshMaheshwari <vedanshm15@gmail.com>
2024-11-29 18:00:20 +0530

    arrayIntersection

--- 0. Basic Concepts/Arrays/arrayIntersection.cpp
+++ 0. Basic Concepts/Arrays/arrayIntersection.cpp
@@ -2,10 +2,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+// NOT OPTIMIZED
 vector<int> findArrayIntersection(vector<int>&arr1, int n,vector<int>&arr2, int m){
-
     
+    vector <int> ans;
+
+    for (int i = 0; i < n; i++){
+        int element = arr1[i];
+        for (int j = 0; i < m; j++){
+            if(element <arr2[j] ){
+                break;
+            }
+            if(element == arr2[j]){
+                ans.push_back(element);
+                arr2[j] = -12241;
+                break;
+            } 
+        }
+    }
+
+    return ans;
 }
 
 #include <iostream>
@@ -26,20 +42,23 @@ int main() {
         cin >> arr2[j];
     }
 
+
+    res = findArrayIntersection(arr1,n,arr2,m);
+
     int i= 0,j = 0;
 
-    while(i < n && j < m){
-        if(arr1[i] == arr2[j]){
-            res.push_back(arr1[i]);
-            i++, j++;
-        }
-        else if (arr1[i] < arr2[j]) {
-            i++;
-        }
-        else{
-            j++;
-        }
-    }
+    // while(i < n && j < m){
+    //     if(arr1[i] == arr2[j]){
+    //         res.push_back(arr1[i]);
+    //         i++, j++;
+    //     }
+    //     else if (arr1[i] < arr2[j]) {
+    //         i++;
+    //     }
+    //     else{
+    //         j++;
+    //     }
+    // }
 
     for (int k = 0; k < res.size(); k++) {
         cout << res[k] << " ";
