63835ea3dfab3385eb0a00f1fc275c1608617f94
VedanshMaheshwari <vedanshm15@gmail.com>
2024-12-03 18:21:17 +0530

    tripletSum

--- 0. Basic Concepts/Arrays/tripletSum.cpp
+++ 0. Basic Concepts/Arrays/tripletSum.cpp
@@ -9,15 +9,13 @@ vector<vector<int>> findTriplets(vector<int>arr, int n, int K) {
     for(int i = 0; i < arr.size(); i++){
         for(int j=i+1; j < arr.size(); j++){
             for(int k=j+1 ; k < arr.size(); k++){
-                if(arr[i]+ arr[j]+ arr[k] == K){
+                if(arr[i]+ arr[j]+ arr[k] == K && (i < j < k)){
                     temp.push_back(arr[i]);
                     temp.push_back(arr[j]);
                     temp.push_back(arr[k]);
                     ans.push_back(temp);
 
-					for (int a = 0; a < temp.size(); a++){
-						cout << temp[a] << " ";
-					}	
+
 								
                 }
 				cout << endl ;
