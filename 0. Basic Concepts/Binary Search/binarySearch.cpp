a092a90fb3fd443e1d65a0b17434d660f807383d
VedanshMaheshwari <vedanshm15@gmail.com>
2025-01-08 20:48:37 +0530

    Binary Search

---
+++ 0. Basic Concepts/Binary Search/binarySearch.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+
+int binarySearch(int arr[], int size, int key){
+
+    int start = 0;
+    int end = size -1;
+    int mid = start + (end - start)/2;
+
+    while(start <= end){
+
+        if(arr[mid] == key){
+            return mid;
+        }
+
+        else if (key > arr[mid] ){
+            start = mid + 1;
+        }
+
+        else if (key < arr[mid]){
+            end = mid - 1;
+        }
+
+        mid = start + (end - start)/2;
+    }  
+    return -1;
+}
+
+
+int main(){
+    int num;
+    cout << "Enter the number of elements in Array: ";
+    cin >> num ;
+    int array[num];
+
+    for (int i = 0; i < num ; i++){
+        cout<< "Enter the "<< i + 1 << " element: ";
+        cin >> array[i];
+    }
+
+    int index = binarySearch(array, num, 297);
+    cout << "The index of the key is: " << index;
+}
