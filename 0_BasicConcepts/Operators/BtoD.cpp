29980d42468bb1e9236915fa7bd413b65950b86a
VedanshMaheshwari <vedanshm15@gmail.com>
2025-06-12 15:59:23 +0530

    Binary to Decimal

--- 0_BasicConcepts/Operators/BtoD.cpp
+++ 0_BasicConcepts/Operators/BtoD.cpp
@@ -8,16 +8,16 @@ int main(){
     cin >> num ;
 
     int i = 0;
-    int bn = 0;
+    int binaryNumber = 0;
 
     int n = num;
     while (num != 0){
-        int ld = num % 10;
-        bn = (ld * pow(2,i) ) +  bn;
+        int lastDigit = num % 10;
+        binaryNumber = (lastDigit * pow(2,i) ) +  binaryNumber;
         i ++ ;
         num = num / 10;
     }
 
-    cout <<"Decimal: "<< bn << endl;
+    cout <<"Decimal: "<< binaryNumber << endl;
 
 }
---
+++ tryingStuff.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    float a = 2, b = 3, c = 4;
+    cout << c*a/b << endl;
+}
