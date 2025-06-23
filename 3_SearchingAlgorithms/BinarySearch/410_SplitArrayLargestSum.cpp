362c219730d37a03626d78427537c96d863b7842
VedanshMaheshwari <vedanshm15@gmail.com>
2025-06-23 22:41:16 +0530

    LC 410

--- 3_SearchingAlgorithms/BinarySearch/410_SplitArrayLargestSum.cpp
+++ 3_SearchingAlgorithms/BinarySearch/410_SplitArrayLargestSum.cpp
@@ -1,23 +1,46 @@
-    bool ifPossible(vector<int>& nums, int k, int mid) {
-        
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+
+    bool ifPossi(vector<int>&nums,int k, int mid){
+        int eleSum = 0;
+        int splits = 1;
+        for(int num : nums){
+            if(eleSum + num <= mid){
+                eleSum = eleSum + num;
+            }else{
+                splits++;
+                if(splits > k ){
+                    return false;
+                }
+                eleSum = 0;
+                eleSum = eleSum + num;
+            }
+        }
+        return true;
     }
+
+
 
     int splitArray(vector<int>& nums, int k) {
         int start = 0;
         int end = 0;
-        // int start = *max_element(nums.begin(), nums.end());
-        // int end = accumulate(nums.begin(), nums.end(), 0);
-        for (int num : nums) {
-            start = max(start, num); // same as max_element
-            end += num;              // same as accumulate
+        for(int num : nums){
+            start = max(start, num);
+            end = end + num;
         }
+        int splits = 0;
         int ans = -1;
-
         while(start <= end){
-            int mid = start + (end - start) / 2;
-            if(ifPossible(nums, k, mid)){
+    
+            int mid = start + (end - start)/2;
+
+            if(ifPossi(nums,k,mid)){
                 ans = mid;
-                end = mid - 1; // try smaller maximum sum
+                end = mid - 1;
             }
             else{
                 start = mid + 1;
@@ -25,3 +48,4 @@
         }
         return ans;
     }
+};
