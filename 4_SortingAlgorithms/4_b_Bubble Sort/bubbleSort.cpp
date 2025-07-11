862ebb7d5e78df56f81c3f18b090e712fe420bb7
VedanshMaheshwari <vedanshm15@gmail.com>
2025-07-11 12:17:31 +0530

    Bubble Sort

--- 4_SortingAlgorithms/4_b_Bubble Sort/bubbleSort.cpp
+++ 4_SortingAlgorithms/4_b_Bubble Sort/bubbleSort.cpp
@@ -4,13 +4,25 @@
 using namespace std;
 
 void bubbleSort(vector<int>& arr, int n){
-
+int a = 0;
     for(int i = 1; i < n; i++){
+        bool swapped = false;
         for(int j = 0;j < n-i ;j++){
+            cout << a ;
             if(arr[j] > arr[j+1]){
                 swap(arr[j],arr[j+1]);
+                swapped = true;
             }
+            a++;
+        }
+        if(swapped == false){
+            break;
         }
     }
+}
+
+int main(){
+    vector<int> arr = {1, 2, 3, 4, 5};
+    bubbleSort(arr,arr.size());
 }
 
