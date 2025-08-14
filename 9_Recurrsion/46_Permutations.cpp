e58717d77818738dfd5ba7f6b9f1b10fa97e0e84
VedanshMaheshwari <vedanshm15@gmail.com>
2025-08-14 21:47:39 +0530

    LC 2264

---
+++ 9_Recurrsion/46_Permutations.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    
+    void solve(vector<int> nums, vector<vector<int>>& answer, int index){
+        //base case
+        if(index >= nums.size()){
+            answer.push_back(nums);
+            return;
+        }
+
+        for(int i = index; i < nums.size(); i++){
+            swap(nums[index],nums[i]);
+            solve(nums,answer,index+1);
+        }
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> answer;
+        int index = 0;
+        solve(nums, answer, index);
+        return answer;
+    }
+};
