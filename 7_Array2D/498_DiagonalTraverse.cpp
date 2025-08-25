811b253e050ed3297a497589444097deaad70b8c
VedanshMaheshwari <vedanshm15@gmail.com>
2025-08-25 23:33:28 +0530

    LC 498

---
+++ 7_Array2D/498_DiagonalTraverse.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        
+        int row = mat.size();
+        int col = mat[0].size();
+        int total = row*col;
+        vector<int> ans;
+        
+        int startRow = 0;
+        int startCol = 0;
+        bool goingUp = true;
+
+        while(ans.size() < total){
+            ans.push_back(mat[startRow][startCol]);
+            
+            if(goingUp){
+                if(startCol == col - 1){
+                    startRow ++;
+                    goingUp = false;
+                }else if(startRow == 0){
+                    startCol++;
+                    goingUp = false;
+                }else{
+                    startRow--;
+                    startCol++;
+                }
+            }
+            else{
+                if(startRow == row - 1){
+                    startCol ++;
+                    goingUp = true;
+                }else if(startCol == 0){
+                    startRow++;
+                    goingUp = true;
+                }else{
+                    startRow++;
+                    startCol--;
+                }
+            }
+        }
+        return ans;
+    }
+};
