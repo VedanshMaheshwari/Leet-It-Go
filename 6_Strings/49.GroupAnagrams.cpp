eb2d6100e9221f8f0a735b3659debecd4b889a85
VedanshMaheshwari <vedanshm15@gmail.com>
2025-09-17 23:35:01 +0530

    LC 49

--- 6_Strings/49.GroupAnagrams.cpp
+++ 6_Strings/49.GroupAnagrams.cpp
@@ -23,3 +23,34 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+      
+        vector<vector<string>> answer;
+        
+        unordered_map<string, vector<string>> map;
+
+        for(auto s : strs){
+            vector<int>freq (26,0);
+            for(char c : s){
+                freq[c-'a']++;
+            }
+            string key;
+
+            for(int x : freq){
+                key = key + "#" + to_string(x);
+            }
+
+            map[key].push_back(s);
+        }
+
+        for(auto a: map){
+            answer.push_back(a.second);
+        }
+
+        return answer;
+    }
+};
