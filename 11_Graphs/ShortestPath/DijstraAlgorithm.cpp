c90428c1ec3d4d5a259f7be738679cd108a42371
VedanshMaheshwari <vedanshm15@gmail.com>
2025-10-07 08:45:36 +0530

    Dijkstra Alhorithm

---
+++ 11_Graphs/ShortestPath/DijstraAlgorithm.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+vector<int> dijkstra(vector<vector<int>> &vec, int vertices, int edges, int source) {
+    // Write your code here.
+
+    unordered_map<int, list<pair<int,int>>>adj;
+    for(int i = 0; i < edges; i++){
+        int u = vec[i][0];
+        int v = vec[i][1];
+        int w = vec[i][2];
+
+        adj[u].push_back({w,v});
+        adj[v].push_back({w,u});
+    }
+
+    set<pair<int,int>> st;
+    vector<int>distance(vertices);
+
+    for(int i = 0; i < vertices; i++){
+        distance[i] = INT_MAX;
+    }
+
+    distance[source] = 0;
+
+    st.insert({0,source});
+
+    while(!st.empty()){
+        auto top = *(st.begin());
+        int distNode = top.first;
+        int nodeTop = top.second;
+
+        st.erase(st.begin());
+
+        for(auto neigh : adj[nodeTop]){
+            if(distNode + neigh.first < distance[neigh.second]){
+                auto record = st.find({distance[neigh.second], neigh.second});
+
+                if(record != st.end()){
+                    st.erase(record);
+                }  
+
+                distance[neigh.second] = distNode + neigh.first;    
+                st.insert({distance[neigh.second], neigh.second});   
+            }
+        }
+        
+    }
+
+    return distance;
+}
+
