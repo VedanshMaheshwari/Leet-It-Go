de7edff32db96b5bbaee8fb95099684f74751727
VedanshMaheshwari <vedanshm15@gmail.com>
2025-10-21 12:59:02 +0530

    LC 141

--- 12_LinkedList/141_LinkedListCycle.cpp
+++ 12_LinkedList/141_LinkedListCycle.cpp
@@ -28,3 +28,27 @@ public:
         return false;
     }
 };
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == NULL){
+            return false;
+        }
+
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while (fast != NULL && fast->next != NULL){
+            fast = fast->next->next;
+            //if(fast != NULL) fast = fast->next;
+            slow = slow->next;
+            if(slow == fast){
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+};
