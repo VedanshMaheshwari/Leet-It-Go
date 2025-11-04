67c97031605e5089931c5fe46ecacc9d2825c07d
VedanshMaheshwari <vedanshm15@gmail.com>
2025-11-04 22:17:37 +0530

    Bubble Sort

--- 4_SortingAlgorithms/4_b_Bubble Sort/bubbleSort.cpp
+++ 4_SortingAlgorithms/4_b_Bubble Sort/bubbleSort.cpp
@@ -21,8 +21,23 @@ int a = 0;
     }
 }
 
+void bubbleSort(vector<int>& arr){
+    int n = arr.size();
+    for(int i = n-1; i >= 1; i--){
+        for(int j = 0; j < n; j ++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
+
 int main(){
-    vector<int> arr = {1, 2, 3, 4, 5};
-    bubbleSort(arr,arr.size());
+    vector<int> arr = {11, 20, 13, 40, 15};
+    bubbleSort(arr);
+
+    for(int num : arr){
+        cout << num << " ";
+    }
 }
 
--- 4_SortingAlgorithms/4_b_Bubble Sort/tempCodeRunnerFile.cpp
+++ 4_SortingAlgorithms/4_b_Bubble Sort/tempCodeRunnerFile.cpp
@@ -1,1 +1,17 @@
-    // bubbleSortRecursion(v,v.size());
+void bubbleSort(vector<int>& arr, int n){
+// int a = 0;
+//     for(int i = 1; i < n; i++){
+//         bool swapped = false;
+//         for(int j = 0;j < n-i ;j++){
+//             // cout << a ;
+//             if(arr[j] > arr[j+1]){
+//                 swap(arr[j],arr[j+1]);
+//                 swapped = true;
+//             }
+//             // a++;
+//         }
+//         if(swapped == false){
+//             break;  
+//         }
+//     }
+// }
