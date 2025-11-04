9b5750fc5dc086682ed2c221d6365f9863f5568b
VedanshMaheshwari <vedanshm15@gmail.com>
2025-11-04 23:09:07 +0530

    Insertion Sort

--- 4_SortingAlgorithms/4_c_InsertionSort/insertionSort.cpp
+++ 4_SortingAlgorithms/4_c_InsertionSort/insertionSort.cpp
@@ -35,3 +35,14 @@ void insertionSort(int n, vector<int> &arr){
         arr[j+1] = temp;
     }
 }
+
+
+void insertionS(vector<int> &arr){
+    for(int i = 0; i < arr.size(); i++){
+        int j = i;
+        while( j > 0 && arr[j-1] > arr[j]){
+            swap(arr[j], arr[j-1]);
+            j--;
+        }
+    }
+}
