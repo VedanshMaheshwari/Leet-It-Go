bd65d136f84a07e3cf9d1b8869281083b792b1be
VedanshMaheshwari <vedanshm15@gmail.com>
2025-11-05 22:54:14 +0530

    Merge Sort

--- 4_SortingAlgorithms/4_d_MergeSort/912_SortAnArrayMergeSort.cpp
+++ 4_SortingAlgorithms/4_d_MergeSort/912_SortAnArrayMergeSort.cpp
@@ -42,6 +42,36 @@ public:
         }
     }
 
+        void merge(vector<int>& nums, int start, int end){
+        int mid = start + (end - start)/2;
+        int left = start;
+        int right = mid + 1;
+        
+        vector<int>temp;
+        
+        while(left <= mid && right <= end){
+            if(nums[left] < nums[right]){
+                temp.push_back(nums[left]);
+                left++;
+            }else{
+                temp.push_back(nums[right]);
+                right++;
+            }
+        }
+
+        while(left <= mid){
+            temp.push_back(nums[left++]);
+        }
+        
+        while(right <= end){
+            temp.push_back(nums[right++]);
+        }
+
+        for(int i = 0; i < temp.size(); i++) {
+            nums[start + i] = temp[i];
+        }
+    }
+
     void mergeSort(vector<int> &array, int start, int end){
         if(start >= end) return;
         int mid = (start + end) / 2;
