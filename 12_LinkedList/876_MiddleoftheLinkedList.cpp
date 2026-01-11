ae4787ea3bed703e5fbf4d172f1543cba77388b3
VedanshMaheshwari <vedanshm15@gmail.com>
2026-01-11 20:45:39 +0530

    LC 876

--- 12_LinkedList/876_MiddleoftheLinkedList.cpp
+++ 12_LinkedList/876_MiddleoftheLinkedList.cpp
@@ -35,3 +35,17 @@ public:
         return temp;
     }
 };
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while(fast != NULL && fast->next != NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+};
