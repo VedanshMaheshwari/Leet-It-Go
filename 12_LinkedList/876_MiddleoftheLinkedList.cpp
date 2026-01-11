cd01c62db8546afac8af4ef53bb0348549edb678
VedanshMaheshwari <vedanshm15@gmail.com>
2026-01-11 20:30:53 +0530

    LC 876

---
+++ 12_LinkedList/876_MiddleoftheLinkedList.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if (head == NULL) return NULL;
+
+        ListNode* temp = head;
+        int count = 0;
+
+        // count nodes
+        while (temp != NULL) {
+            count++;
+            temp = temp->next;
+        }
+
+        int mid = count / 2;   // second middle automatically
+        temp = head;
+
+        while (mid--) {
+            temp = temp->next;
+        }
+
+        return temp;
+    }
+};
