11f79624e37bcced0521ef4538de18313a725d84
VedanshMaheshwari <vedanshm15@gmail.com>
2026-01-13 13:58:24 +0530

    LC 234

--- 12_LinkedList/234_PalindromeLL.cpp
+++ 12_LinkedList/234_PalindromeLL.cpp
@@ -1,3 +1,8 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+ 
  struct ListNode {
       int val;
       ListNode *next;
@@ -51,6 +56,50 @@ public:
                 return false;
             head1 = head1->next;
             head2 = head2->next;
+        }
+
+        return true;
+    }
+};
+
+class Solution {
+public:
+
+    ListNode *reverseRecurr(ListNode *head){
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+
+        ListNode *newNode = reverseRecurr(head->next);
+
+        ListNode *front = head->next;
+        front->next = head;
+        head->next = NULL;
+
+        return newNode;
+    }
+
+    bool isPalindrome(ListNode* head) {
+        ListNode * slow = head;
+        ListNode * fast = head;
+    
+        while(fast->next != NULL && fast->next->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* secondHalf = reverseRecurr(slow->next);
+
+        // Step 3: Compare both halves
+        ListNode* first = head;
+        ListNode* second = secondHalf;
+
+        while(second != NULL){
+            if(first->val != second->val){
+                return false;
+            }
+
+            first = first->next;
+            second = second->next;
         }
 
         return true;
