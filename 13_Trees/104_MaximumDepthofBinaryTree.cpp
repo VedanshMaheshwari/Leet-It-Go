f2524ff773e54ef24c204c18c7da80120a6f864c
VedanshMaheshwari <vedanshm15@gmail.com>
2026-01-15 10:05:57 +0530

    LC 104

---
+++ 13_Trees/104_MaximumDepthofBinaryTree.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<stack>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+
+        queue<TreeNode *> q;
+        q.push(root);
+        int level = 0;
+
+        while(!q.empty()){
+            int size = q.size();
+            level++;
+
+            for(int i = 0; i < size; i++){
+                TreeNode* node = q.front();
+                q.pop();
+                if(node->left != NULL){
+                    q.push(node->left);
+                }
+                if(node->right != NULL){
+                    q.push(node->right);
+                }
+            }
+        }
+
+        return level;
+    }
+};
